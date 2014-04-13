4c952d3211b003d55a980841508240d4ce69c655
Xhacker Liu <liu.dongyuan@gmail.com>
2014-04-12 23:14:24 -0700

    Basic working sender and receiver.

---
+++ receiver.py
@@ -0,0 +1,21 @@
+from socket import socket, AF_INET, SOCK_DGRAM
+from sys import argv
+
+socket
+
+if __name__ == "__main__":
+    dest_addr = argv[1]
+    dest_port = int(argv[2])
+    dest = (dest_addr, dest_port)
+    listen_addr = argv[3]
+    listen_port = int(argv[4])
+    listen = (listen_addr, listen_port)
+
+    send_sock = socket(AF_INET, SOCK_DGRAM)
+    recv_sock = socket(AF_INET, SOCK_DGRAM)
+
+    recv_sock.bind(listen)
+
+    while True:
+        message, address = recv_sock.recvfrom(4096)
+        print message
---
+++ sender.py
@@ -0,0 +1,24 @@
+from socket import socket, AF_INET, SOCK_DGRAM
+from sys import argv
+
+socket
+
+if __name__ == "__main__":
+    dest_addr = argv[1]
+    dest_port = int(argv[2])
+    dest = (dest_addr, dest_port)
+    listen_addr = argv[3]
+    listen_port = int(argv[4])
+    listen = (listen_addr, listen_port)
+    content = argv[5]
+
+    send_sock = socket(AF_INET, SOCK_DGRAM)
+    recv_sock = socket(AF_INET, SOCK_DGRAM)
+
+    recv_sock.bind(listen)
+
+    send_sock.sendto(content, dest)
+
+    while True:
+        message, address = recv_sock.recvfrom(4096)
+        print message
